278ac4dec5ca9a08aa87b591f7c1f386976ea0b0
Ashwini Vangari <ashwinivangari2610@gmail.com>
2023-10-20 23:54:41 +0530

    Day=17

---
+++ Day_17/Ugly_no.java
@@ -0,0 +1,17 @@
+package For_Practice.Day_17;
+
+public class Ugly_no {
+    public boolean isUgly(int n) {
+        if (n<=0) return false;
+        while(n%2==0){
+                n/=2;
+            }
+       while(n%3==0){
+           n/=3;
+       }
+       while(n%5==0){
+           n/=5;
+       }
+       return n==1;
+   }
+}
